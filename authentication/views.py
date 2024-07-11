c0ad9b790ae4946c6f371f3dc90e2b914bce06e2
Debby Trinita <debby.trinita>
2024-07-11 22:53:02 +0700

    feat:login

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+
+
+@csrf_exempt
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            auth_login(request, user)
+            # Redirect to a success page.
+            return JsonResponse({
+            "status": True,
+            "message": "Successfully Logged In!"
+            # Insert any extra data if you want to pass data to Flutter
+            }, status=200)
+        else:
+            return JsonResponse({
+            "status": False,
+            "message": "Failed to Login, Account Disabled."
+            }, status=401)
+
+    else:
+        return JsonResponse({
+        "status": False,
+        "message": "Failed to Login, check your username/password."
+        }, status=401)
+
+
+@csrf_exempt
+def logout(request):
+	if request.user.is_authenticated or ['loggedIn']:
+		if request.user.is_authenticated:
+			auth_logout(request)
+		return JsonResponse({"status" : "logged out"}, status=200)
+	return JsonResponse({"status": "Not yet authenticated"}, status =403)
+
