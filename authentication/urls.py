598ae34b3412963b2167e91d863f6a8300612242
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2024-07-12 02:42:38 +0700

    feat: register html

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,5 @@ from authentication.views import register
 app_name = 'auth'
 
 urlpatterns = [
-    # path('', tracker, name='index'),
     path('register', register, name='register'),
-    # path('add_footprint', add_footprint, name='add_footprint'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -11,25 +11,44 @@ from authentication.models import RegisteredUser
 def register(request):
     if (request.method == "POST"):
         try:
-            username = request.POST.get("username")
-            password = request.POST.get("password")
-            nama = request.POST.get("nama")
-            peran = request.POST.get("peran")
-            domisili = request.POST.get("domisili")
-            agama = request.POST.get("agama")
-            umur = request.POST.get("umur")
-            golongan_darah = request.POST.get("golongan_darah")
-            kondisi_ibu = request.POST.get("kondisi_ibu")
-            umur_bayi = request.POST.get("umur_bayi")
-            jenis_kelamin = request.POST.get("jenis_kelamin")
+            username = request.POST["username"]
+            password = request.POST["password"]
+            nama = request.POST["nama"]
+            peran = request.POST["peran"]
+            domisili = request.POST["domisili"]
+            agama = request.POST["agama"]
+            umur = request.POST["umur"]
+            if umur == '':
+                umur = None
+            
+            try: 
+                golongan_darah = request.POST["golongan_darah"]
+            except:
+                golongan_darah = None
+
+            try: 
+                kondisi_ibu = request.POST["kondisi_ibu"]
+            except:
+                kondisi_ibu = None
+
+            try: 
+                umur_bayi = request.POST["umur_bayi"]
+            except:
+                umur_bayi = None
+
+            try:
+                jenis_kelamin = request.POST["jenis_kelamin"]
+            except:
+                jenis_kelamin = None
 
             user = User.objects.create_user(username=username, password=password)
             pengguna_baru = RegisteredUser.objects.create(user=user, nama = nama, peran = peran, domisili = domisili, agama = agama, umur = umur, golongan_darah = golongan_darah, kondisi_ibu = kondisi_ibu, umur_bayi = umur_bayi, jenis_kelamin_bayi = jenis_kelamin)
             
             pengguna_baru.save()
+            return render(request, "login.html")
             return JsonResponse({"instance": "user Dibuat"}, status=200)
         
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=400)
-     
+    
     return render(request, "register.html")
