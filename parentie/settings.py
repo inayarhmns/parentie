c4285fadceff38056d29805476e515d37409bb64
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2024-07-11 21:30:15 +0700

    feat: connect to db

--- parentie/settings.py
+++ parentie/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'parentie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.wizcnocbwwsmhwzhcxcx',
+        'PASSWORD': 'GARUDAsishacks2024',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
