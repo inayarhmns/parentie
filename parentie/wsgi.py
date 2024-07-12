77ce21b585af3c56b162ecf5277a5a493bb24707
inayarhmns <inaya.rahmanisa@gmail.com>
2024-07-13 02:21:58 +0700

    fix:wsgi

--- parentie/wsgi.py
+++ parentie/wsgi.py
@@ -10,7 +10,5 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parentie.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
 application = get_wsgi_application()
